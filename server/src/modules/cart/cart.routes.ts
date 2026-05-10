24912f161a7becc635cdb88a24ccdc4b22141b48
LuisFPamplona <luispamplona2702@gmail.com>
2026-05-10 15:54:24 -0300

    refactor: removed cart-items route, and centralized in cart routes

--- server/src/modules/cart/cart.routes.ts
+++ server/src/modules/cart/cart.routes.ts
@@ -1,9 +1,27 @@
 import { Router } from "express";
 import { authMiddleware } from "../../middlewares/authMiddleware";
-import { getCart } from "./cart.controller";
+import {
+  addToCart,
+  getCart,
+  removeFromCart,
+  updateCartItem,
+} from "./cart.controller";
+import { validateBody } from "../../middlewares/validateBody";
+import { addToCartSchema, updateCartItemSchema } from "./cart.schema";
 
 const router = Router();
 
 router.get("/", authMiddleware, getCart);
+
+router.post("/", authMiddleware, validateBody(addToCartSchema), addToCart);
+
+router.delete("/:id", authMiddleware, removeFromCart);
+
+router.patch(
+  "/:id",
+  authMiddleware,
+  validateBody(updateCartItemSchema),
+  updateCartItem,
+);
 
 export default router;
--- server/src/modules/cart/cart.service.ts
+++ server/src/modules/cart/cart.service.ts
@@ -30,3 +30,103 @@ export const getCartService = async (userId: string) => {
 
   return { ...cart, totalPrices };
 };
+
+export const addToCardService = async (
+  userId: string,
+  productId: string,
+  quantity: number,
+) => {
+  const cart = await prisma.cart.findUnique({
+    where: { userId },
+  });
+
+  if (!cart) {
+    throw new Error("Cart not found for user");
+  }
+
+  const existingCartItem = await prisma.cartItem.findUnique({
+    where: {
+      cartId_productId: {
+        cartId: cart.id,
+        productId: productId,
+      },
+    },
+  });
+
+  if (existingCartItem) {
+    const updatedCardItem = await prisma.cartItem.update({
+      where: { id: existingCartItem.id },
+      data: { quantity: existingCartItem.quantity + quantity },
+    });
+    return updatedCardItem;
+  }
+
+  const newCartItem = await prisma.cartItem.create({
+    data: {
+      cartId: cart.id,
+      productId,
+      quantity,
+    },
+  });
+
+  return newCartItem;
+};
+
+export const removeFromCartService = async (
+  productId: string,
+  userId: string,
+) => {
+  const cart = await prisma.cart.findUnique({
+    where: { userId },
+    select: { id: true },
+  });
+
+  if (!cart) {
+    throw new AppError("Cart not found", 404);
+  }
+
+  const product = await prisma.cartItem.findFirst({
+    where: { cartId: cart.id, productId },
+  });
+
+  if (!product) {
+    throw new AppError("Product not founded in cart", 404);
+  }
+
+  const deletedProduct = await prisma.cartItem.delete({
+    where: { id: product.id },
+  });
+
+  return deletedProduct;
+};
+
+export const updateCartItemService = async (
+  productId: string,
+  userId: string,
+  quantity: number,
+) => {
+  const cart = await prisma.cart.findUnique({
+    where: { userId },
+  });
+
+  if (!cart) {
+    throw new AppError("Cart not found", 404);
+  }
+
+  const cartItem = await prisma.cartItem.findFirst({
+    where: { cartId: cart.id, productId },
+  });
+
+  if (!cartItem) {
+    throw new AppError("Product not founded in cart", 404);
+  }
+
+  const updatedProduct = await prisma.cartItem.update({
+    where: { id: cartItem.id },
+    data: {
+      quantity,
+    },
+  });
+
+  return updatedProduct;
+};
